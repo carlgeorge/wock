8d4beb4bce66b939f57d1311bf150dbac8307469
carl.george <carl.george@rackspace.com>
2015-02-11 15:02:20 -0600

    update help strings

--- wock/__init__.py
+++ wock/__init__.py
@@ -1,31 +1,39 @@
+import os
 import click
 from .utils import ContextObj
 
 
 @click.group()
-@click.option('--pkgname')
-@click.option('--release', envvar='WOCK')
-@click.option('--architecture', default='x86_64')
+@click.option('--pkgname',
+              help='Desired package name.\t[current directory name]')
+@click.option('--release', envvar='WOCK',
+              help='Desired release.\t[{}]'.format(os.environ.get('WOCK')))
+@click.option('--architecture', default='x86_64',
+              help='Desired architecture.\t[x86_64]')
 @click.pass_context
 def cli(context, pkgname, release, architecture):
+    ''' A wrapper for mock. '''
     context.obj = ContextObj(pkgname, release, architecture)
 
 
 @click.command()
 @click.pass_obj
 def init(cobj):
+    ''' Initialize a chroot. '''
     cobj.init()
 
 
 @click.command()
 @click.pass_obj
 def clean(cobj):
+    ''' Purge the chroot. '''
     cobj.clean()
 
 
 @click.group()
 @click.pass_obj
 def build(cobj):
+    ''' Build a (s)rpm. '''
     cobj.do_checks()
 
 
@@ -45,6 +53,7 @@ def rpm(cobj):
 @click.argument('packages', nargs=-1, type=click.Path(exists=True))
 @click.pass_obj
 def install(cobj, packages):
+    ''' Install rpm inside chroot. '''
     cobj.install(packages)
 
 
@@ -52,6 +61,7 @@ def install(cobj, packages):
 @click.argument('command', default='bash')
 @click.pass_obj
 def shell(cobj, command):
+    ''' Run command interactively inside chroot. '''
     cobj.shell(command)
 
 
